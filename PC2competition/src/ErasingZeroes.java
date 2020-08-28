aa1c5ca73c5a4240cf1ca3fac70040cf7bb6615b
nwosaemeka <mykev.paul@gmail.com>
2020-08-28 09:27:00 +0800

    updated the question source

--- PC2competition/src/ErasingZeroes.java
+++ PC2competition/src/ErasingZeroes.java
@@ -1,22 +1,28 @@
+//https://codeforces.com/problemset/problem/1303/A
+
+/*You are given a string s. Each character is either 0 or 1.
+You want all 1's in the string to form a contiguous subsegment. For example, if the string is 0, 1, 00111 or 01111100,
+then all 1's form a contiguous subsegment, and if the string is 0101, 100001 or 11111111111101, then this condition is not met.
+You may erase some (possibly none) 0's from the string. What is the minimum number of 0's that you have to erase?
+*/
+
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class ErasingZeroes {
 	public static void main(String[]args)throws IOException{
-		
+
 			File file = new File("hh.txt");
 			Scanner sc = new Scanner (file);
-			
+
 			int num = Integer.valueOf(sc.nextLine());
-			
-//			This program takes a number containing 1's and 0's then count the number of 0's between the 1's
-//			010011 return 2 because 01__11 while 1111000 return 0 because there is no 0 between all the 1's
+
 			for(int i =0; i<num; i++){
 				String s = sc.nextLine();
-				int x = s.indexOf('1');
-				int y = s.lastIndexOf('1');
-				
+				int x = s.indexOf('1');//first index of 1
+				int y = s.lastIndexOf('1'); // last index of 1
+
 				int count =0;
 				for(int j = x+1; j<y; j++){
 					if(s.charAt(j) =='0'){
--- PC2competition/src/Satay.java
+++ PC2competition/src/Satay.java
@@ -1,3 +1,4 @@
+//https://www.acmicpc.net/problem/15838
 import java.io.*;
 import java.util.Scanner;
 public class Satay {
@@ -6,27 +7,27 @@ public class Satay {
 		double beef = 1.0 - ((24.0+8.0)/85);
 		double lamb = 1.2 - ((32.0+8.0)/85);
 		double rice = 0.8 -0.2;
-		
+
 		File file = new File ("satay.txt");
-		
+
 		Scanner input = new Scanner(file);
 		int nline = Integer.valueOf(input.nextLine());
-		
+
 		while(input.hasNextLine()){
 		double price = 0.0;
 		for(int i =1; i <= nline; i++){
 			String line = input.nextLine();
 			String[]temp = line.split(" ");
-			int cp = Integer.valueOf(temp[0]);
-			int bp = Integer.valueOf(temp[1]);
-			int lp = Integer.valueOf(temp[2]);
-			int rp = Integer.valueOf(temp[3]);
-		   price = price + (cp*chic)+(beef*bp)+(lamb*lp)+(rice*rp);
-		} 
+			int chickenPrice = Integer.valueOf(temp[0]);
+			int beefPrice = Integer.valueOf(temp[1]);
+			int lambPrice = Integer.valueOf(temp[2]);
+			int ricePrice = Integer.valueOf(temp[3]);
+		   price = price + (chickenPrice*chic)+(beef*beefPrice)+(lamb*lambPrice)+(rice*ricePrice);
+		}
 		System.out.printf("Case %01d: RM%.2f\n" ,nline,price);
-		
+
 		nline = Integer.valueOf(input.nextLine());
-		
+
 		}
 		input.close();
 		}
